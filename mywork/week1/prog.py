d7411ce0b372ecceb304988971c14917678554d9
unknown <netofr@gmail.com>
2024-10-05 17:51:06 +0100

    initial

--- mywork/week1/prog.py
+++ mywork/week1/prog.py
@@ -1,7 +1,7 @@
 import csv
 
 FILENAME= "data.csv"
-DATADIR = "C:\Users\Chris\Desktop\PFDA\mywork\week1\"
+DATADIR = "../week1/"
 
 with open (DATADIR + FILENAME, "rt") as fp:
     reader = csv.reader(fp, delimiter=",")
